62244965fa59afd12635db7f349672d4e0a71387
Bartosz Lubański <bartosz.lubansky@gmail.com>
2026-07-21 12:37:02 +0200

    Add request timeout to KtorQuoteApi

--- shared/src/commonMain/kotlin/com/example/quotekmp/network/QuoteApi.kt
+++ shared/src/commonMain/kotlin/com/example/quotekmp/network/QuoteApi.kt
@@ -3,6 +3,7 @@ package com.example.quotekmp.network
 import com.example.quotekmp.model.Quote
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.json
@@ -16,6 +17,9 @@ class KtorQuoteApi : QuoteApi {
     private val client = HttpClient {
         install(ContentNegotiation) {
             json(Json { ignoreUnknownKeys = true })
+        }
+        install(HttpTimeout) {
+            requestTimeoutMillis = 10_000
         }
     }
 
